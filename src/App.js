98c43608135ab1dadb5b3b859c6975896b7d4f36
Excel231 <psymaster44@gmail.com>
2022-05-03 20:46:36 +0300

    Components are sorted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
+import Header from "./headerSection/Header";
+import Main from "./mainSection/Main";
+import Footer from "./footerSection/Footer";
 import "./styles.css"
 
 function App() {
