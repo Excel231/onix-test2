9eba3a19ce9decbe84f1c358e46161456960073b
Excel231 <psymaster44@gmail.com>
2022-05-15 16:08:06 +0300

    Title list beutify

--- src/pages/biography/TitleListOfParameters.js
+++ src/pages/biography/TitleListOfParameters.js
@@ -2,13 +2,16 @@ import React from "react";
 import ListOfParameters from "./ListOfParameters";
 
 const TitleListOfParameters = ({handleClick}) => {
-    return (<ListOfParameters key={0}
-                              fullName={"Full Name"}
-                              age={"Age"}
-                              birthYear={"Birth Year"}
-                              weight={"Weight"}
-                              belts={[{beltName: "Belt Name", year: "Year"}]}
-                              handleClick={handleClick}/>
+    return (
+        <ListOfParameters
+            key={0}
+            fullName={"Full Name"}
+            age={"Age"}
+            birthYear={"Birth Year"}
+            weight={"Weight"}
+            belts={[{beltName: "Belt Name", year: "Year"}]}
+            handleClick={handleClick}
+        />
     );
 }
 
