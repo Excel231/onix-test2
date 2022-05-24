16b4dc2c48b4607df7e9badc1f4a464dcb2c454e
Excel231 <psymaster44@gmail.com>
2022-05-24 17:19:44 +0300

    Counter updated

--- src/сomponents/DiscountTimer/DiscountTimer.js
+++ src/сomponents/DiscountTimer/DiscountTimer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./DiscountTimerView";
 import DiscountTimerView from "./DiscountTimerView";
 
-const DISCOUNT_END_TIME = new Date(2022, 4, 25, 0, 0, 0);
+const DISCOUNT_END_TIME = new Date(2022, 4, new Date().getDate() + 2, 0, 0, 0);
 
 class DiscountTimer extends React.Component {
     state = {
