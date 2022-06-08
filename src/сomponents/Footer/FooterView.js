3a6854f023edb232eebfa73745d80b39a8dc58f1
Excel231 <psymaster44@gmail.com>
2022-06-08 15:09:36 +0300

    Minor style changes

--- src/сomponents/Footer/FooterView.js
+++ src/сomponents/Footer/FooterView.js
@@ -9,18 +9,24 @@ class FooterView extends React.Component {
             <footer>
                 <h3 className="author-name">by Ivan Honcharenko</h3>
                 <h3>Also check {linkText}</h3>
-                <FooterLink onButtonEnter={changeSignUpText}
-                            linkTitle="GitHub"
-                            footerLink={gitHubLink}
-                            faClasses="fa-brands fa-github fa-2xl"/>
-                <FooterLink onButtonEnter={changeSignUpText}
-                            linkTitle="Design"
-                            footerLink={designLink}
-                            faClasses="fa-solid fa-object-group fa-2xl"/>
-                <FooterLink onButtonEnter={changeSignUpText}
-                            linkTitle="SH++"
-                            footerLink={shLink}
-                            faClasses="fa-solid fa-school fa-2xl"/>
+                <FooterLink
+                    onButtonEnter={changeSignUpText}
+                    linkTitle="GitHub"
+                    footerLink={gitHubLink}
+                    faClasses="fa-brands fa-github fa-2xl"
+                />
+                <FooterLink
+                    onButtonEnter={changeSignUpText}
+                    linkTitle="Design"
+                    footerLink={designLink}
+                    faClasses="fa-solid fa-object-group fa-2xl"
+                />
+                <FooterLink
+                    onButtonEnter={changeSignUpText}
+                    linkTitle="SH++"
+                    footerLink={shLink}
+                    faClasses="fa-solid fa-school fa-2xl"
+                />
             </footer>
         );
     }
