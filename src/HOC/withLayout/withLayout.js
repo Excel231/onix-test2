5baebae4519a17e54c235b2f01a0989c714032d8
Excel231 <psymaster44@gmail.com>
2022-06-17 20:10:43 +0300

    Rewrite HOC as class component

--- src/HOC/withLayout/withLayout.js
+++ src/HOC/withLayout/withLayout.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Header from '../../сomponents/Header/Header';
 import Footer from '../../сomponents/Footer/Footer';
 
-const withLayout = (WrappedComponent) => {
-  return (
-    <>
-      <Header />
-      {WrappedComponent}
-      <Footer />
-    </>
-  );
-};
+function withLayout(WrappedComponent) {
+  return class extends React.Component {
+    render() {
+      return (
+        <>
+          <Header />
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <WrappedComponent {...this.props} />
+          <Footer />
+        </>
+      );
+    }
+  };
+}
 
 export default withLayout;
