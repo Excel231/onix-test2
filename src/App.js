7747410947087ca70c18cce12aa44b02d35c2293
Excel231 <psymaster44@gmail.com>
2022-06-28 15:52:59 +0300

    Router moved to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Routes } from 'react-router';
 import Homepage from './pages/Homepage/Homepage';
 import BiographyPage from './pages/BiographyPage/BiographyPage';
@@ -7,13 +7,15 @@ import ThemeColorProvider from './layout/ThemeColorProvider/ThemeColorProvider';
 
 const App = () => {
   return (
-    <ThemeColorProvider>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/onix-test2" element={<Homepage />} />
-        <Route path="/biography" element={<BiographyPage />} />
-      </Routes>
-    </ThemeColorProvider>
+    <BrowserRouter>
+      <ThemeColorProvider>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/onix-test2" element={<Homepage />} />
+          <Route path="/biography" element={<BiographyPage />} />
+        </Routes>
+      </ThemeColorProvider>
+    </BrowserRouter>
   );
 };
 
