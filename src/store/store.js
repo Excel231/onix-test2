834156724c8face40a699b0b209216575bc3fc6d
Excel231 <psymaster44@gmail.com>
2022-07-11 19:38:57 +0300

    Store fixed

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import commentsReducer from './comments/commentsReducer';
-import boxersReducer from './boxers/boxersReducer';
 
 const store = configureStore({
   reducer: {
-    commentsReducer,
-    boxersReducer
+    commentsReducer
   }
 });
 
